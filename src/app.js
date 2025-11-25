515b3c1ea71810bb0a6647dbb819d0484097c166
Ruben Alba Gonzalez <39350323+Ruben-Alba-Gonzalez@users.noreply.github.com>
2025-11-25 17:54:47 +0000

    New excuse button working

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import "./assets/img/4geeks.ico";
 
 //VARIABLES
 let articulo = ['Mi','Tu','El'];
-let quien = ['perro', 'abuela', 'cartero', 'pajaro', 'primillo chico'];
+let quien = ['perro', 'abuelo', 'cartero', 'pajaro', 'primillo chico'];
 let accion = ['se comio', 'robo', 'rompio', 'escondio'];
 let que = ['mis deberes', 'mi telefono', 'las llaves del coche', 'las llaves de la moto'];
 let cuando = ['justo antes de clase', 'mientras dormia', 'cuando estaba en clase', 'mientras comia', 'cuando estaba en el baño', 'cuando estaba leyendo'];
@@ -20,9 +20,15 @@ function valorAleatorio(maximo){
 }
 
 function generarExcusa(){
-  objetivo.innerHTML= `${articulo[valorAleatorio(articulo.length)]} ${quien[valorAleatorio(quien.length)]} ${accion[valorAleatorio(accion.length)]} ${que[valorAleatorio(que.length)]} ${cuando[valorAleatorio(cuando.length)]}`;
-}
+   objetivo.innerHTML= `${articulo[valorAleatorio(articulo.length)]} ${quien[valorAleatorio(quien.length)]} ${accion[valorAleatorio(accion.length)]} ${que[valorAleatorio(que.length)]} ${cuando[valorAleatorio(cuando.length)]}`;
+  }
 
+
+//EVENTOS 
 window.onload = function() {
+  generarExcusa();
+};
+
+boton.onclick = function(){
   generarExcusa();
 };
